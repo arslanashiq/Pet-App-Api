1e2caa185ebc25d4dd923ef4f7591fd8ed2cda21
unknown <ashiqarslan66@gmail.com>
2022-09-20 10:57:56 +0500

    gitignore file

--- src/databases/mongoose.js
+++ src/databases/mongoose.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true,  useUnifiedTopology: true,useCreateIndex: true}).then(() => {
-    console.log('successfully connected to the database');
-}).catch(err => {
-    console.log('error connecting to the database');
+mongoose
+  .connect(
+    "mongodb+srv://adminadmin:adminadmin@cluster0.m9ykp.mongodb.net/eshop-database?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+  )
+  .then(() => {
+    console.log("successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("error connecting to the database", err);
     process.exit();
-});
+  });
 module.exports = { mongoose };
-
-
-
