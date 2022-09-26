abd6a1ef80c6c4a5c39987308d02534590a1ca49
unknown <ashiqarslan66@gmail.com>
2022-09-26 17:18:40 +0500

    pet list by user id

--- src/databases/mongoose.js
+++ src/databases/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
+mongoose.connect("mongodb+srv://adminadmin:adminadmin@cluster0.m9ykp.mongodb.net/eshop-database?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
     console.log('successfully connected to the database');
 }).catch(err => {
     console.log('error connecting to the database');
